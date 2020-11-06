93d670e4142445a4fb11e9a3583bfffaa8962c31
farahmh <61236001+farahmh@users.noreply.github.com>
2020-11-06 17:13:31 +0000

    created scrabble and scrabble score classes

---
+++ run_scrabble.py
@@ -0,0 +1,19 @@
+class Scrabble():
+
+    def __init__(self):
+        self.one_point = ['A', 'E', 'I', 'O', 'U', 'L', 'N', 'R', 'S', 'T']
+        self.two_point = ['D','G']
+        self.three_point = ['B', 'C', 'M', 'P']
+        self.four_point = ['F', 'H', 'V', 'W', 'Y']
+        self.five_point = ['K']
+        self.eight_point = ['J', 'X']
+        self.ten_point = ['Q','Z']
+
+
+class Points(Scrabble):
+    def __init__(self):
+        super().init()
+    
+
+    def count_score(self, string_input):
+        
